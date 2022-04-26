06d3c9ce5a34a9a9d26af7bd5820dbf29ae18864
Haoyuan Lin <linhaoyuan@bytedance.com>
2022-04-26 20:27:50 +0800

    fix

--- baccarat.py
+++ baccarat.py
@@ -1,12 +1,12 @@
 # This is a python file to show how the game works
 import random
 
+
 class Game:
     CARDS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
     VALUE = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0]
     OUTCOME = ['Player wins', 'Banker wins', 'Tie']
 
-    
     def __init__(self):
         print('Game started')
 
@@ -26,11 +26,12 @@ class Game:
             random.choice(self.CARDS)
         ]
         banker_hand = [
-            random.choice(self. CARDS),
+            random.choice(self.CARDS),
             random.choice(self.CARDS)
         ]
 
         player_score = self.compute_score(player_hand)
+
         banker_score = self.compute_score(banker_hand)
 
         # print('Player has cards:\t' + player_hand[0] + '\t' + player_hand[1])
@@ -44,8 +45,10 @@ class Game:
             #     return self.OUTCOME[banker_score > player_score]
             # else:
             #     return self.OUTCOME[2]
+            print('player: ', player_score)
+            print('banker: ', banker_score)
             if banker_score < player_score:
-                return self.OUTCOME[0], 2*bet
+                return self.OUTCOME[0], 2 * bet
             elif banker_score > player_score:
                 return self.OUTCOME[1], 0
             else:
@@ -60,10 +63,10 @@ class Game:
 
             # Determine if banker needs a third card
             if (banker_score == 6 and player_third in [6, 7]) or \
-            (banker_score == 5 and player_third in irange(4, 7)) or \
-            (banker_score == 4 and player_third in irange(2, 7)) or \
-            (banker_score == 3 and player_third != 8) or \
-            (banker_score in [0, 1, 2]):
+                    (banker_score == 5 and player_third in irange(4, 7)) or \
+                    (banker_score == 4 and player_third in irange(2, 7)) or \
+                    (banker_score == 3 and player_third != 8) or \
+                    (banker_score in [0, 1, 2]):
                 banker_hand.append(random.choice(self.CARDS))
                 # print('Banker gets a third card:\t' + banker_hand[2])
 
@@ -75,12 +78,13 @@ class Game:
         # Compute the scores again and return the outcome
         player_score = self.compute_score(player_hand)
         banker_score = self.compute_score(banker_hand)
-
+        print('player: ', player_score)
+        print('banker: ', banker_score)
         # print('Player has final score of\t' + str(player_score))
         # print('Banker has final score of\t' + str(banker_score))
 
         if banker_score < player_score:
-            return self.OUTCOME[0], 2*bet
+            return self.OUTCOME[0], 2 * bet
         elif banker_score > player_score:
             return self.OUTCOME[1], 0
         else:
--- baccarat_test.py
+++ baccarat_test.py
@@ -1,43 +1,54 @@
 from baccarat import Game
 
-rounds = 10
-games = 500
-amount = 5000
+amount = 30000
 OUTCOME = ['Player wins', 'Banker wins', 'Tie']
-strategy = [1,2,3,6]
+strategy = [1, 1, 2, 4]
 stage = 0
+amount_record = []
 amount_records = []
 game = Game()
 bet_unit = 500
+banker_hand = 0
+player_hand = 0
+tie_hand = 0
+
 # print(game.play(10)[0])
-
-for r in range(0,rounds):
-    amount_records.append([])
-    print(amount_records)
-    print('round:',r)
-    stage = 0
+for i in range(1, 10000):
     bet = strategy[stage]
-    for g in range(0,games):
-        if amount <= bet_unit:
-            print('broken')
-            break
-        print('bet:',bet)
-        
-        amount_records[r].append(amount)
-        amount -= bet*bet_unit
-        res, payback = game.play(bet*bet_unit)
-        print('res:',res)
-        amount += payback
+    print('round ', i)
+    if amount <= bet_unit:
+        print('broken')
+        break
+    if amount >= 40000:
+        print('wins')
+        break
+    print('bet:', bet)
+    amount_record.append(amount)
+    amount -= bet * bet_unit
+    res, payback = game.play(bet * bet_unit)
+    print('res:', res)
 
-        print('amount:',amount)
-        if res == OUTCOME[0]:
-            stage = 0
-        elif res == OUTCOME[1]:
-            stage = (stage+1)%len(strategy)
-        bet = strategy[stage]
-        
-    print('Final amount:', amount)
-    amount = 5000
+    # 计算奖金
+    amount += payback
 
+    # 如果是banker赢了
+    if payback == 0:
+        banker_hand += 1
+        # 如果是player赢了
+    elif payback == 2 * bet * bet_unit:
+        player_hand += 1
+        # 如果是tie
+    else:
+        tie_hand += 1
 
-print(amount_records)
+    if res == OUTCOME[0]:
+        stage = 0
+    elif res == OUTCOME[1]:
+        stage = (stage + 1) % len(strategy)
+    print('now you have: ', amount)
+
+print('Final amount:', amount)
+print('banker rate', banker_hand / i * 100)
+print('player rate', player_hand / i * 100)
+print('tie rate', tie_hand / i * 100)
+print(banker_hand / i * 100 + player_hand / i * 100 + tie_hand / i * 100)
